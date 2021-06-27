b947b907f71aebb83d3b2c1167c49a5e27708322
kurrbanov <justnick1312@gmail.com>
2021-06-27 19:42:11 +0300

    Add the search and getting the info about user.

--- src/telesystem/views.py
+++ src/telesystem/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
+
+from .models import *
 
 
 def login_page(request):
@@ -34,11 +37,48 @@ def index(request):
 
 @login_required(login_url='login')
 def find(request):
+    CITIES = [
+        ('1', 'Екатеринбург'),
+        ('2', 'Елабуга'),
+        ('3', 'Зеленодольск'),
+        ('4', 'Иннополис'),
+        ('5', 'Казань'),
+        ('6', 'Краснодар'),
+        ('7', 'Москва'),
+        ('8', 'Санкт-Петербург'),
+        ('9', 'Сочи')
+    ]
+    
+    RATES = [
+        ('1', 'Ультра'),
+        ('2', 'Smart'),
+        ('3', 'Безлимитище'),
+        ('4', 'Тарифище')
+    ]
+    
     if request.method == 'POST':
-        phone_number = request.POST['phone_number']
+        try:
+            phone_number = PhoneNumber.objects.get(number=request.POST['phone_number'])
+        except ObjectDoesNotExist:
+            print('yes')
+            return render(request, 'subscriber_information.html', {'found': False})
 
+        city = ''
+        for c in CITIES:
+            if c[0] == phone_number.subscriber.cities:
+                city = c[1]
+        rate = ''
+        for r in RATES:
+            if r[0] == phone_number.rate:
+                rate = r[1]
 
-        return render(request, 'subscriber_information.html', {})
+        return render(request, 'subscriber_information.html', {
+            'subscriber': phone_number.subscriber,
+            'phone_info': phone_number,
+            'city': city,
+            'rate': rate,
+            'found': True
+        })
 
     return render(request, 'subscriber_information.html', {})
 
